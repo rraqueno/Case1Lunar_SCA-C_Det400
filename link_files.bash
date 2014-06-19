279a04bc679038ed79d82cec3a82b0ecd9924814
U-homestar\rvrpci <rvrpci@homestar.(none)>
2014-06-19 11:06:03 -0400

    This versions contains the state prior to the major change needed to allow intercase matrix concatenation. This means the current code will have to be modified so that the resulting output matrices will have the same elements in the ghost contribution dimension.

---
+++ link_files.bash
@@ -0,0 +1,5 @@
+cp -uv MODIS/A2013-309-2110-LAC_SST_geoL8TOA_B11 MODIS.img
+cp -uv MODIS/A2013-309-2110-LAC_SST_geoL8TOA_B11.hdr MODIS.img.hdr
+cp -uv ImageRadLatsLons_Column_1521_SCA-C_Det400_case1_309.img L8.img
+cp -uv ImageRadLatsLons_Column_1521_SCA-C_Det400_case1_309.hdr L8.hdr
+cp -uv ImageRadLatsLons_Column_1521_SCA-C_Det400_case1_309.bll L8.bll
